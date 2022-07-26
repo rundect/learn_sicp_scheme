1e1aff36509c64adeb922a0677511c4f79a28834
rundect <93350655+rundect@users.noreply.github.com>
2022-07-26 22:08:10 +0300

    Solve 1.8

--- 1. Построение абстракций с помощью процедур/1.1. Элементы программирования/1.1.7.-problem-1.8.ss.rkt
+++ 1. Построение абстракций с помощью процедур/1.1. Элементы программирования/1.1.7.-problem-1.8.ss.rkt
@@ -1,15 +1,15 @@
 #lang sicp
 
-(define (square x) (* x x))
+(define (cube x) (* x x x))
 
-(define (average x y)
-  (/ (+ x y) 2))
+(define (third x y)
+  (/ (+ (/ x (* y y)) (* 2 y)) 3))
 
 (define (improve guess x)
-  (average guess (/ x guess)))
+  (third x guess))
 
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.000001))
+  (< (abs (- (cube guess) x)) 0.00001))
 
 
 (define (sqrt-iter guess x)
@@ -21,28 +21,4 @@
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-(sqrt 9)
-(sqrt (+ 100 37))
-(sqrt (+ (sqrt 2) (sqrt 3)))
-(square (sqrt 1000))
-
-
-; Exercise 1.7 Solution
-(define (better-good-enough? prev-guess guess)
-  (< (abs (- guess prev-guess)) 0.000001))
-
-
-(define (bettersqrt-iter prev-guess guess x)
-  (if (better-good-enough? prev-guess guess)
-      guess
-      (bettersqrt-iter guess
-                       (improve guess x)
-                       x)))
-
-(define (better-sqrt x)
-  (bettersqrt-iter 0 1.0 x))
-
-(better-sqrt 9)
-(better-sqrt (+ 100 37))
-(better-sqrt (+ (better-sqrt 2) (better-sqrt 3)))
-(square (better-sqrt 1000))
+(sqrt 27)
