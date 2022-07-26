cbf82b3b66522928f49281dae45aa9245949fa6f
rundect <93350655+rundect@users.noreply.github.com>
2022-07-26 21:42:07 +0300

    Fix structure of files

--- 1. Построение абстракций с помощью процедур/1.1. Элементы программирования/1.1.7.-problem-1.8.ss.rkt
+++ 1. Построение абстракций с помощью процедур/1.1. Элементы программирования/1.1.7.-problem-1.8.ss.rkt
@@ -9,7 +9,7 @@
   (average guess (/ x guess)))
 
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (square guess) x)) 0.000001))
 
 
 (define (sqrt-iter guess x)
@@ -27,21 +27,22 @@
 (square (sqrt 1000))
 
 
-(define (better-good-enough? prev-guess guess x)
-  (< (abs (- guess prev-guess)) 0.001))
+; Exercise 1.7 Solution
+(define (better-good-enough? prev-guess guess)
+  (< (abs (- guess prev-guess)) 0.000001))
 
 
 (define (bettersqrt-iter prev-guess guess x)
-  (if (better-good-enough? prev-guess guess x)
+  (if (better-good-enough? prev-guess guess)
       guess
-      (bettersqrt-iter (improve guess x)
-                 x)))
+      (bettersqrt-iter guess
+                       (improve guess x)
+                       x)))
 
 (define (better-sqrt x)
   (bettersqrt-iter 0 1.0 x))
 
 (better-sqrt 9)
-(sqrt (+ 100 37))
-(sqrt (+ (sqrt 2) (sqrt 3)))
-(square (sqrt 1000))
-
+(better-sqrt (+ 100 37))
+(better-sqrt (+ (better-sqrt 2) (better-sqrt 3)))
+(square (better-sqrt 1000))
